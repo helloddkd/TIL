2ac15d094f4d6742c5cf2e489da44bcc1d8e62da
helloddkd <img9672@daum.net>
2019-01-18 15:58:08 +0900

    project

--- 01_python/getdata/get_lotto.py
+++ 01_python/getdata/get_lotto.py
@@ -8,5 +8,3 @@ got = requests.get(URL)
 
 print(got)
 
-
-http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=555226eeca79dde09ad09563c98d1523&targetDt=20190113&weekGb=%220%22
